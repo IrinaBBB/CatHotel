ec4a403ad24ec1f8aa742186ebae4bff87ae6ffc
Irina <balkarovairina@gmail.com>
2023-04-08 10:28:06 +0200

    Add HotelRoom Entity

---
+++ DataAccess/Data/HotelRoom.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataAccess.Data
+{
+    public class HotelRoom
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Occupancy { get; set; }
+        [Required]
+        public double RegularRate { get; set; }
+        public string Details { get; set; }
+        public string SqM { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+        public string UpdatedBy { get; set; }
+        public DateTime UpdatedDate { get; set; }
+
+    }
+}
